146700ceb21136f2dce86b1e4f1ede772f9e66ec
ebaytheway <lafleurr4141@gmail.com>
2023-04-18 00:02:04 +0300

    feat:add new data

--- src/userdata.jsx
+++ src/userdata.jsx
@@ -6,12 +6,23 @@ export const userData = [
         watched:106,
         likes:29,
         lists:2,
-        reviews:[
+        favorites:[
             {
-                film_img:"",
-                filmName:"",
-                
-            }
+                film_img:"https://a.ltrbxd.com/resized/film-poster/4/6/0/1/5/5/460155-1917-0-500-0-750-crop.jpg?v=9eb1d95df5",
+                film_name:"1917"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/4/3/2/0/0/4/432004-sound-of-metal-0-150-0-225-crop.jpg?v=289acd955b",
+                film_name:"Sound Of Metal"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/sm/upload/yx/yw/v4/el/mhFXxB76LVgJBIfJM1sAuVBtVKv-0-150-0-225-crop.jpg?v=6ba4c6154d",
+                film_name:"Se7en"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/5/1/7/0/0/51700-12-angry-men-0-150-0-225-crop.jpg?v=b8aaf291a9",
+                film_name:"12 Angry Men"
+            },
         ]
     },
     {
@@ -21,6 +32,24 @@ export const userData = [
         watched:240,
         likes:40,
         lists:6,
+        favorites:[
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/4/3/3/8/6/3/433863-the-lighthouse-0-150-0-225-crop.jpg?v=ee97005537",
+                film_name:"Lighthouse"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/5/1/7/0/0/51700-12-angry-men-0-150-0-225-crop.jpg?v=b8aaf291a9",
+                film_name:"12 Angry Men"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/sm/upload/58/tf/z6/gw/qKQIjErYaN149hZRP5Vgf8RoC7S-0-150-0-225-crop.jpg?v=72ee5db942",
+                film_name:"Zodiac",
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/3/4/8/9/1/4/348914-the-batman-0-150-0-225-crop.jpg?v=ec12a8b7ce",
+                film_name:"The Batman",
+            }
+        ]
     },
     {
         userName:"Brenzalor",
@@ -29,6 +58,25 @@ export const userData = [
         watched:22,
         likes:16,
         lists:0,
+        favorites:[
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/5/1/7/0/0/51700-12-angry-men-0-150-0-225-crop.jpg?v=b8aaf291a9",
+                film_name:"12 Angry Men"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/4/3/3/8/6/3/433863-the-lighthouse-0-150-0-225-crop.jpg?v=ee97005537",
+                film_name:"Lighthouse"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/5/6/0/7/8/7/560787-spider-man-no-way-home-0-150-0-225-crop.jpg?v=a336d4f40c",
+                film_name:"Spider-Man:No Way Home"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/sm/upload/de/kp/63/hd/dWg33ektXuHmxjSjEulwDPTWbC2-0-150-0-225-crop.jpg?v=df40f228cb",
+                film_name:"Cars"
+            }
+
+        ]
     },
     {
         userName:"meybuzdayi",
@@ -37,6 +85,24 @@ export const userData = [
         watched:20,
         likes:0,
         lists:0,
+        favorites:[
+            {
+                film_img:null,
+                film_name:null,
+            },
+            {
+                film_img:null,
+                film_name:null,
+            },
+            {
+                film_img:null,
+                film_name:null,
+            },
+            {
+                film_img:null,
+                film_name:null,
+            },
+        ]
     },
     {
         userName:"emr3rden",
@@ -45,6 +111,24 @@ export const userData = [
         watched:217,
         likes:88,
         lists:17,
+        favorites:[
+            {
+                film_img:"https://a.ltrbxd.com/resized/sm/upload/nx/8b/vs/gc/cDbNAY0KM84cxXhmj8f0dLWza3t-0-150-0-225-crop.jpg?v=49eed12751",
+                film_name:"Dune"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/1/1/7/6/2/1/117621-interstellar-0-150-0-225-crop.jpg?v=7ad89e6666",
+                film_name:"Interstellar",
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/4/6/1/4/4/46144-sherlock-holmes-0-150-0-225-crop.jpg?v=92f4a1b4a1",
+                film_name:"Sherlock Holmes"
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/3/1/8/2/3/31823-yahsi-bati-the-ottoman-cowboys-0-150-0-225-crop.jpg?v=f6fd903227",
+                film_name:"Yahşi Batı"
+            }
+        ]
         
     },
     {
@@ -54,6 +138,24 @@ export const userData = [
         watched:86,
         likes:16,
         lists:1,
+        favorites:[
+            {
+                film_img:null,
+                film_name:null,
+            },
+            {
+                film_img:null,
+                film_name:null,
+            },
+            {
+                film_img:null,
+                film_name:null,
+            },
+            {
+                film_img:null,
+                film_name:null,
+            },
+        ]
     },
     {
         userName:"ivankerimovic",
@@ -62,6 +164,23 @@ export const userData = [
         watched:1017,
         likes:1025,
         lists:129,
+        favorites:[
+            {
+                film_img:"https://a.ltrbxd.com/resized/sm/upload/t3/qn/vs/p5/s5fH3GqFcHbi2F0NSBSh4KRNTc0-0-150-0-225-crop.jpg?v=01880125bb",
+                film_name:"The Thing",
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/sm/upload/03/67/3s/tc/terminator-2-judgment-day-original-0-150-0-225-crop.jpg?v=12f5752b5c",
+                film_name:"Terminator 2: Judgment Day",
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/5/1/9/9/4/51994-heat-0-150-0-225-crop.jpg?v=54dce97dc6",
+                film_name:"Heat",
+            },
+            {
+                film_img:"https://a.ltrbxd.com/resized/film-poster/4/5/1/5/1/2/451512-the-last-black-man-in-san-francisco-0-150-0-225-crop.jpg?v=7ed1dacd29",
+                film_name:"The Last Black Man in San Francisco",
+            },
+        ]
     }
-    
 ]
